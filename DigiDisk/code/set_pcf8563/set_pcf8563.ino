b279d2f6f83d93e873f62552148a3bcf02a9ee4c
potblitd <93311104+potblitd@users.noreply.github.com>
2024-01-26 19:32:39 +0800

    Update set_pcf8563.ino

--- DigiDisk/code/set_pcf8563/set_pcf8563.ino
+++ DigiDisk/code/set_pcf8563/set_pcf8563.ino
@@ -11,16 +11,10 @@ void setup() {
   // setup rtc
   rtc.initClock();
   //day, weekday, month, century, year
-  rtc.setDate(24, 3, 1, 20, 24);
-  //hr, min, sec
-  rtc.setTime(9, 9, 0);
-
+  rtc.setDate(26, 5, 2, 20, 24);
+  //hour, minute, second
+  rtc.setTime(19, 31, 0);
 }
 
 void loop() {
-  Serial.print(rtc.formatTime());
-  Serial.print("\r\n");
-  Serial.print(rtc.formatDate());
-  Serial.print("\r\n");
-  delay(1000);
 }
